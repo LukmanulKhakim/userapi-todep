47a819a9b03b4691d4bcb1ceb89ad2ac6f1b1efd
lukmanul <klukmanul33@gmail.com>
2022-10-16 23:55:12 +0700

    recreate config

--- config/config.go
+++ config/config.go
@@ -4,17 +4,16 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/gommon/log"
 )
 
 type AppConfig struct {
-	DBPort    uint
-	DBUser    string
-	DBPwd     string
-	DBHost    string
-	DBName    string
-	JWTSecret string
+	Driver   string
+	Name     string
+	Address  string
+	Port     int
+	Username string
+	Password string
 }
 
 func NewConfig() *AppConfig {
@@ -28,25 +27,44 @@ func NewConfig() *AppConfig {
 }
 
 func initConfig() *AppConfig {
-	var app AppConfig
+	var defaultConfig AppConfig
 
-	err := godotenv.Load("config.env")
+	// err := godotenv.Load("config.env")
+	// if err != nil {
+	// 	log.Error("config error :", err.Error())
+	// 	return nil
+	// }
+	//SECRET := os.Getenv("SECRET")
+	cnv, err := strconv.Atoi(os.Getenv("SERVERPORT"))
 	if err != nil {
-		log.Error("config error :", err.Error())
+		log.Fatal("Cannot parse port variable")
 		return nil
 	}
+	//SERVERPORT := int16(cnv)
+	defaultConfig.Name = os.Getenv("Name")
+	defaultConfig.Username = os.Getenv("Username")
+	defaultConfig.Password = os.Getenv("Password")
+	defaultConfig.Address = os.Getenv("Address")
 
-	app.DBUser = os.Getenv("DB_USER")
-	app.DBPwd = os.Getenv("DB_PWD")
-	app.DBHost = os.Getenv("DB_HOST")
-	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	cnv, err = strconv.Atoi(os.Getenv("Port"))
 	if err != nil {
-		log.Error("config error :", err.Error())
+		log.Fatal("Cannot parse DB Port variable")
 		return nil
 	}
-	app.DBPort = uint(port)
-	app.DBName = os.Getenv("DB_NAME")
-	app.JWTSecret = os.Getenv("JWT_SECRET")
+	defaultConfig.Port = cnv
 
-	return &app
+	return &defaultConfig
+	// app.DBUser = os.Getenv("DB_USER")
+	// app.DBPwd = os.Getenv("DB_PWD")
+	// app.DBHost = os.Getenv("DB_HOST")
+	// port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	// if err != nil {
+	// 	log.Error("config error :", err.Error())
+	// 	return nil
+	// }
+	// app.DBPort = uint(port)
+	// app.DBName = os.Getenv("DB_NAME")
+	// app.JWTSecret = os.Getenv("JWT_SECRET")
+
+	// return &app
 }
--- utils/database/mysql.go
+++ utils/database/mysql.go
@@ -10,23 +10,35 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitDB(c *config.AppConfig) *gorm.DB {
-	str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		c.DBUser,
-		c.DBPwd,
-		c.DBHost,
-		c.DBPort,
-		c.DBName,
-	)
+func InitDB(cfg *config.AppConfig) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", cfg.Username, cfg.Password, cfg.Address, cfg.Port, cfg.Name)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
 	if err != nil {
-		log.Error("db config error :", err.Error())
-		return nil
+		log.Fatal("Cannot connect to DB")
 	}
+
 	migrateDB(db)
 	return db
 }
+
+// func InitDB(c *config.AppConfig) *gorm.DB {
+// 	str := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+// 		c.DBUser,
+// 		c.DBPwd,
+// 		c.DBHost,
+// 		c.DBPort,
+// 		c.DBName,
+// 	)
+
+// 	db, err := gorm.Open(mysql.Open(str), &gorm.Config{})
+// 	if err != nil {
+// 		log.Error("db config error :", err.Error())
+// 		return nil
+// 	}
+// 	migrateDB(db)
+// 	return db
+// }
 
 func migrateDB(db *gorm.DB) {
 	db.AutoMigrate(&rUser.User{})
